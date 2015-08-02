aa3701249f7e9bad8735e49f4e433f5f9eaed296
devmetal <devmetal91@gmail.com>
2015-08-02 20:36:42 +0200

    Create error handling in streams

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var babelify = require('babelify');
 var browserSync = require('browser-sync').create();
 var less = require('gulp-less');
 var seq = require('run-sequence');
+var Notification = require('node-notifier');
+var util = require('gulp-util');
 
 var options = {
     debug:true,
@@ -27,17 +29,25 @@ gulp.task('js', function(){
     return browserify(options)
         .transform(babelify)
         .bundle()
+        .on('error', function(err) {
+            util.log(util.colors.red('Error'), err.message);
+            this.emit('end');
+        })
         .pipe(source('app.js'))
         .pipe(buffer())
         .pipe(gulp.dest('./public/js'))
-        .pipe(browserSync.reload({stream:true, one:true}));
+        .pipe(browserSync.reload({stream:true, one:true}))
 });
 
 gulp.task('less', function(){
     return gulp.src('./src/less/main.less')
         .pipe(less())
+        .on('error', function(err) {
+            util.log(util.colors.red('Error'), err.message);
+            this.emit('end');
+        })
         .pipe(gulp.dest('./public/css'))
-        .pipe(browserSync.reload({stream:true, one:true}));
+        .pipe(browserSync.reload({stream:true, one:true}))
 });
 
 gulp.task('serve', function(done) {
